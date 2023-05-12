ca9d8ba380e69e6c8c5ecc0af98865c4aaf1b972
A-Jatana <amitoz2003@hotmail.com>
2023-05-12 16:37:36 -0400

    Created format for portfolio, experience, contact

---
+++ src/components/About.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div name='about' className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white">
+        <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full"> 
+            <div className="pb-8">
+                <p className="text-4xl font-bold inline border-b-4 border-gray-500">About</p>
+            </div>
+
+            <p className="text-xl mt-5">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia quibusdam sit quasi, asperiores commodi neque minima! Quos blanditiis dolorum iusto facere eos ipsa placeat optio, modi impedit nesciunt perspiciatis sapiente reprehenderit vitae quia inventore porro est corrupti explicabo? Doloremque alias veniam atque aliquam animi reiciendis placeat consectetur nisi minus praesentium?
+            </p>
+
+            <br />
+
+            <p className="text-xl">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit quis, ratione delectus nesciunt dolorem magnam non. Maiores, soluta numquam sed minus eum consequatur rerum, ipsa delectus autem recusandae non facilis est fugit magni et voluptate. Maiores, dolor hic. Non, corporis nesciunt! Officiis debitis unde quis cumque, voluptatum maiores iure aspernatur!
+            </p>
+        </div>
+        
+    </div>
+  )
+}
+
+export default About
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import HeroImage from '../assets/html.png'
+import HeroImage from '../assets/newImage.jpg'
 import { MdOutlineKeyboardArrowRight } from 'react-icons/md'
+import { Link } from 'react-scroll'
 
 const Home = () => {
   return (
@@ -19,13 +20,13 @@ const Home = () => {
 
                 <div>
                     {/* we put group so the hover effect is applied to the whole button */}
-                    <button className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500">
+                    <Link to='portfolio' smooth duration={500} className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500">
                         Portfolio
                         <span className="group-hover:rotate-90 duration-300">
                             <MdOutlineKeyboardArrowRight size={25} 
                             className="ml-1"/>
                         </span>
-                    </button>
+                    </Link>
                 </div>
 
             </div>
