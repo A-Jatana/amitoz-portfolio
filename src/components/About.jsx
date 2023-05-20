4b7d14ee215743ba38319584215fcd42f45caba1
A-Jatana <amitoz2003@hotmail.com>
2023-05-20 14:46:54 -0400

    fixed padding issue for mobile

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div name='about' className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white">
-        <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full"> 
+    <div name='about' className="w-full min-h-screen py-20 bg-gradient-to-b from-gray-800 to-black text-white">
+        <div className="max-w-screen-lg mx-auto p-4  text-white"> 
             <div className="pt-16 pb-8">
-                <p className="text-4xl font-bold inline border-b-4 border-gray-500">About</p>
+                <p className="text-4xl font-bold border-b-4 border-gray-500 inline">About</p>
             </div>
 
             <p className="text-xl mt-5">
