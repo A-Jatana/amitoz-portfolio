63672ad58e31f4f9083d98a8b6976cbe222b1d4e
A-Jatana <amitoz2003@hotmail.com>
2023-05-20 01:05:41 -0400

    Updated resume

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -44,7 +44,7 @@ const SocialLinks = () => {
                 Resume <BsFillPersonLinesFill size={30}/>
                 </> 
             ), 
-            href: '/Amitoz Jatana Resume.pdf',
+            href: '/Resume (Amitoz Jatana).pdf',
             style: 'rounded-bl-md',
             download: true, 
             
